be9b5170afa7fbe1dc21717d57404ee42d6bf2f1
sangjin yu <sangjin025@gmail.com>
2025-06-14 03:13:25 +0900

    Header, Footer 컴포넌트 추가

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./Header.module.css";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Header() {
+  const pathname = usePathname();
+
+  return (
+    <header className={styles.header}>
+      <Link href="/" className={styles.logo}>
+        <Image
+          src="/images/netand_logo.png"
+          alt="넷앤드"
+          width={242}
+          height={53}
+        />
+      </Link>
+      <nav className={styles.nav}>
+        <Link
+          href="/main"
+          className={`${styles.navItem} ${
+            pathname === "/main" ? styles.active : ""
+          }`}
+        >
+          메인페이지
+        </Link>
+        <Link
+          href="/maintenance"
+          className={`${styles.navItem} ${
+            pathname === "/maintenance" ? styles.active : ""
+          }`}
+        >
+          정기점검관리
+        </Link>
+        <Link
+          href="/issues"
+          className={`${styles.navItem} ${
+            pathname === "/issues" ? styles.active : ""
+          }`}
+        >
+          이슈관리
+        </Link>
+      </nav>
+      <div className={styles.controls}>
+        <button className={styles.logout}>로그인/회원가입</button>
+        <button className={styles.hamburger}>
+          <Image src="/images/hamburger.png" alt="" width={36} height={25} />
+        </button>
+      </div>
+    </header>
+  );
+}
