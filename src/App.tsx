9cc2c861390fe8a3e62dab57978830eef72c286f
sangjin yu <sangjin025@gmail.com>
2025-07-10 02:46:47 +0900

    컴파일 에러 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 // src/App.tsx
-import React from 'react';
-import Page from './app/signup/page'; // ✅ Page.tsx 임포트
+import React from "react";
+import Page from "./app/signup/page"; // ✅ Page.tsx 임포트
 
-import '@fontsource/inter';
-import '@fontsource/inter/300.css';
-import '@fontsource/inter/700.css';
+import "@fontsource/inter";
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/700.css";
 
 function App() {
   return <Page />; // ✅ 여기서 Page 컴포넌트를 렌더링
