d3491506d1530088d8b254a5697dd2a92fc02821
sangjin yu <sangjin025@gmail.com>
2025-07-10 03:21:01 +0900

    signup 페이지 이동 오류 해결

--- src/app/signUp/Page.tsx
+++ src/app/signUp/Page.tsx
@@ -1,3 +1,14 @@
-import SignUpPage from "./components/SignUpPage";
+import React from "react";
+import SignUpForm from "./components/SignUpForm";
+import styles from "./components/SignUpForm.module.css";
+
+const SignUpPage = () => {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>회원가입</h2>
+      <SignUpForm />
+    </div>
+  );
+};
 
 export default SignUpPage;
